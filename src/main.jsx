a9168bf4e4dad6f355113f3dec8ff2e81cfb9869
athiraek <131427213+athiraek@users.noreply.github.com>
2025-09-02 14:51:28 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import './assets/css/bootstrap.min.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+import "./index.css";
+import "./assets/css/bootstrap.min.css";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter basename="/photography-app">
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
